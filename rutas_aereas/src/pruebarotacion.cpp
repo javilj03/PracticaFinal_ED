c56fa4dad0436b9699c59faa058caac6bd658fda
IvanFrison <ivanfrisonivan@gmail.com>
2023-12-04 19:11:56 +0100

    comentado el main original de pruebarotacion.cpp

--- rutas_aereas/src/pruebarotacion.cpp
+++ rutas_aereas/src/pruebarotacion.cpp
@@ -110,4 +110,25 @@ int main(int argc, char *argv[]) {
 
     //Iout.EscribirImagen(argv[3]);
 
+
+
+    /**
+    int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        cout << "Los parametros son :" << endl;
+        cout << "1.-La Imagen de entrada" << endl;
+        cout << "2.-El angulo de rotación" << endl;
+        cout << "3.-El nombre de la Imagen de salida" << endl;
+        return 0;
+    }
+    Imagen I;
+    I.LeerImagen(argv[1]);
+    double angulo = atof(argv[2]);
+    angulo = angulo * (M_PI) / 180;
+    Imagen Iout = Rota(I, angulo);
+    Iout.EscribirImagen(argv[3]);
+
+}
+
+     */
 }  
